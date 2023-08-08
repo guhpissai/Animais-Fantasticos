2a8ac0a98e883efa087bc2d3d138014ed033730e
guhpissai <gustavopissai3d@gmail.com>
2023-08-08 18:50:59 -0300

    Criando a navegacao por tabs utilizando as imagens dos animais

---
+++ script.js
@@ -0,0 +1,16 @@
+const tabmenu = document.querySelectorAll('.js-tabmenu li')
+const tabcontent = document.querySelectorAll('.js-tabcontent section')
+
+function activeTab(index) {
+  tabcontent.forEach((section) => {
+    section.classList.remove('active')
+  })
+  tabcontent[index].classList.add('active')
+}
+
+tabmenu.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    activeTab(index)
+  })
+})
+
